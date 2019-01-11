8833334605f6b2b42e187cb12f3bbaa470a0cc98
Sean.Duggan <Sean.Duggan@Laptop-SDug2.ward.ie>
2019-01-11 12:09:49 +0000

    Removing individual apps that have been integrated into main app.

--- src/MobileShepherd/MobileShepherd/app/src/main/java/com/mobshep/mobileshepherd/reverseEngineer.java
+++ src/MobileShepherd/MobileShepherd/app/src/main/java/com/mobshep/mobileshepherd/reverseEngineer.java
@@ -1,14 +1,13 @@
 package com.mobshep.mobileshepherd;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.support.v7.widget.Toolbar;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * This file is part of the Security Shepherd Project.
@@ -44,10 +43,10 @@ public class reverseEngineer extends MainActivity implements OnClickListener, Na
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+     //   ActionBarDrawerToggle toggle;
+       // toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+       // drawer.setDrawerListener(toggle);
+        //toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
